58191db2e7f49d3202b9e2999d2f2dbfd9d71322
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-19 23:44:05 -0300

    feat: esboço para buscar comentários

---
+++ src/corrente_pipeline_comentarios/obter_lista_comentarios_corrente.py
@@ -0,0 +1,22 @@
+from src.contexto.contexto import Contexto
+from src.corrente_pipeline_comentarios.corrente import Corrente
+from src.servicos.api_youtube.iapi_youtube import IApiYoutube
+
+
+class ObterListacomentariosCorrente(Corrente):
+
+    def __init__(self, servico_youtube: IApiYoutube):
+        super().__init__()
+
+        self.__servico_youtube = servico_youtube
+
+    def __buscar_comentarios_videos_antigos(self):
+        return ['a', 'b']
+
+    def __buscar_videos_recentes(self):
+        pass
+
+    def executar_processo(self, contexto: Contexto) -> bool:
+        a = self.__buscar_videos_antigos()
+        b =
+        return True
