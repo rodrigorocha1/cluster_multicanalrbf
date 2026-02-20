31b63e2ad433e52e6b7089bd35230dc7cc83b33e
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-20 20:48:31 -0300

    feat: estutrurando passos

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import datetime
 import os
-
 from datetime import datetime
+
 from src.contexto.contexto import Contexto
-from src.corrente_pipeline_comentarios.obter_lista_canais_corrente import ObterListaComentariosCorrente
+from src.corrente_pipeline_comentarios.obter_lista_canais_corrente import ObterListaCanaisCorrente
+from src.corrente_pipeline_comentarios.obter_lista_comentarios_corrente import ObterListacomentariosCorrente
+from src.corrente_pipeline_comentarios.obter_lista_videos_corrente import ObterListaVideosCorrente
+# from src.corrente_pipeline_comentarios.obter_lista_canais_corrente import ObterListaComentariosCorrente
 from src.corrente_pipeline_comentarios.obter_ultima_data_publicacao_corrente import ObterUltimaDataPublicacaoCorrente
 from src.servicos.api_youtube.api_youtube import YoutubeAPI
 
-contexto = Contexto( data_publicacao=datetime.now())
+contexto = Contexto(data_publicacao=datetime.now())
 caminho_banco = os.path.join(os.getcwd(), 'logs', 'logs.db')
 lista_canais = ['@jogatinaepica', '@ChratosGameplay']
 servico_youtube = YoutubeAPI()
 p1 = ObterUltimaDataPublicacaoCorrente(caminho_banco=caminho_banco)
-p2 = ObterListaComentariosCorrente(lista_canais=lista_canais, servico_youtube=servico_youtube)
-p1.set_proxima_corrente(p2)
+p2 = ObterListaCanaisCorrente(lista_canais=lista_canais, servico_youtube=servico_youtube)
+p3 = ObterListaVideosCorrente(lista_canais=lista_canais, servico_youtube=servico_youtube)
+p4 = ObterListacomentariosCorrente(servico_youtube=servico_youtube)
+p1.set_proxima_corrente(p2).set_proxima_corrente(p3).set_proxima_corrente(p4)
 p1.corrente(contexto=contexto)
-
-
