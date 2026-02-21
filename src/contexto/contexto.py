de74da18b6a4822c10b8174eb5989aa589162f03
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-20 21:37:29 -0300

    refactor: tipagem

--- src/contexto/contexto.py
+++ src/contexto/contexto.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Iterable, Any, List, Tuple, Optional, Generator
+from typing import Iterable, Any, List, Optional, Dict
 
 import pandas as pd
 
@@ -10,7 +10,7 @@ class Contexto:
     data_publicacao: Optional[datetime]
     gerador_reviews_steam: Iterable[Any] = field(default_factory=list)
     gerador_comentarios_youtube: Iterable[Any] = field(default_factory=list)
-    lista_id_comentarios: List[Tuple[str, ...]] = field(default_factory=list)
+    lista_id_comentarios: Iterable[Dict[str, Any]] = field(default_factory=list)
     gerador_resposta_comentarios: Iterable[Any] = field(default_factory=list)
     dataframe_original: pd.DataFrame = field(default_factory=pd.DataFrame)
     lista_canais: List[str] = field(default_factory=list)
