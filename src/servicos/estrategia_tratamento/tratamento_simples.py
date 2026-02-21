f89b6dbc682664283cc3d17d774bea9e79f2dde2
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-21 15:24:36 -0300

    fix: tratamento comentários nan

--- src/servicos/estrategia_tratamento/tratamento_simples.py
+++ src/servicos/estrategia_tratamento/tratamento_simples.py
@@ -1,34 +1,46 @@
 import re
 import emoji
+import string
+import unicodedata
+import pandas as pd
 from src.servicos.estrategia_tratamento.itratatamento import ITratamento
-
 
 class TratamentoSimples(ITratamento[str, str]):
 
     @staticmethod
     def __remover_links(comentario: str) -> str:
-        comentario = re.sub(r"http\S+|www\S+|https\S+", "", comentario)
-        return comentario
+        return re.sub(r"http\S+|www\S+|https\S+", "", comentario)
 
     @staticmethod
     def __remover_emoji(comentario: str) -> str:
-        comentario = emoji.replace_emoji(comentario, replace="")
-        return comentario
+        return emoji.replace_emoji(comentario, replace="")
 
     @staticmethod
     def __deixar_letras_minusculas(comentario: str) -> str:
-        comentario = comentario.lower()
-        return comentario
+        return comentario.lower()
 
     @staticmethod
     def __remover_mencoes(comentario: str) -> str:
+        return re.sub(r"@\w+", "", comentario)
 
-        comentario = re.sub(r"@\w+", "", comentario)
-        return comentario
+    @staticmethod
+    def __remover_pontuacao(comentario: str) -> str:
+        return re.sub(rf"[{re.escape(string.punctuation)}]", "", comentario)
 
-    def executar_tratamento(self, comentario: str) -> str:
+    @staticmethod
+    def __remover_acentos(comentario: str) -> str:
+        comentario_normalizado = unicodedata.normalize('NFKD', comentario)
+        comentario_ascii = comentario_normalizado.encode('ASCII', 'ignore').decode('utf-8')
+        return comentario_ascii
+
+    def executar_tratamento(self, comentario) -> str:
+
+        comentario = str(comentario or "")
         comentario = self.__remover_links(comentario)
         comentario = self.__remover_emoji(comentario)
         comentario = self.__remover_mencoes(comentario)
         comentario = self.__deixar_letras_minusculas(comentario)
+        comentario = self.__remover_pontuacao(comentario)
+        comentario = self.__remover_acentos(comentario)
+        comentario = re.sub(r"\s+", " ", comentario).strip()
         return comentario
