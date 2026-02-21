5f925daee48c63b19c55ca24b256b738cb5a6f0d
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-21 15:06:18 -0300

    feat: criação do método de leitura de resposta de comentários e união dos dataframes

--- main_processamento_de_linguagem_natural.py
+++ main_processamento_de_linguagem_natural.py
@@ -12,57 +12,22 @@ from src.corrente_pipeline_comentarios.obter_lista_resposta_comentarios_corrente
     ObterListaRespostaComentariosCorrente
 from src.corrente_pipeline_comentarios.obter_lista_videos_corrente import ObterListaVideosCorrente
 from src.corrente_pipeline_comentarios.obter_ultima_data_publicacao_corrente import ObterUltimaDataPublicacaoCorrente
+from src.corrente_pipeline_comentarios.tratramento_comentarios_corrente import TratamentoComentariosCorrente
 from src.servicos.api_youtube.api_youtube import YoutubeAPI
 from src.servicos.banco.operacoes_banco import OperacoesBancoDuckDb
 from src.servicos.servico_s3.sevicos3 import ServicoS3
 
 contexto = Contexto(data_publicacao=datetime.now())
 caminho_banco = os.path.join(os.getcwd(), 'logs', 'logs.db')
-lista_canais = [
-    # '@jogatinaepica',
-    # '@CKXgameplay',
-    # '@PalaDinXPG',
-    # '@CanaldoVoid',
-    # '@ChratosGameplay',
-    # '@LivesdoChratos',
-    # '@cmdrleonerd',
-    '@BarbosaCities'
 
-]
-servico_youtube = YoutubeAPI()
-servico_s3 = ServicoS3()
 servico_banco_analitico = OperacoesBancoDuckDb()
-p1 = ObterUltimaDataPublicacaoCorrente(
-    caminho_banco=caminho_banco)
-p2 = ObterListaCanaisCorrente(
-    lista_canais=lista_canais,
-    servico_youtube=servico_youtube
-)
-p3 = ObterListaVideosCorrente(
-    lista_canais=lista_canais,
-    servico_youtube=servico_youtube
-)
-p4 = ObterListacomentariosCorrente(
-    servico_youtube=servico_youtube
-)
-p5 = GuardarComentariosS3Corrente(
-    servico_youtube=servico_youtube,
-    servico_banco_analitico=servico_banco_analitico,
-    servico_s3=servico_s3
-)
-p6 = ObterListaRespostaComentariosCorrente(
-    servico_youtube=servico_youtube)
-p7 = GuardarDadosYoutubeRespostaComentariosS3Corrente(
-    servico_banco_analitico=servico_banco_analitico,
-    servico_s3=servico_s3
-)
 
-p8 = FimCadeia(caminho_banco=caminho_banco)
-p1.set_proxima_corrente(p2) \
-    .set_proxima_corrente(p3) \
-    .set_proxima_corrente(p4) \
-    .set_proxima_corrente(p5) \
-    .set_proxima_corrente(p6) \
-    .set_proxima_corrente(p7) \
-    .set_proxima_corrente(p8)
+p1 = TratamentoComentariosCorrente(operacoes_banco=servico_banco_analitico)
+# p1.set_proxima_corrente(p2) \
+#     .set_proxima_corrente(p3) \
+#     .set_proxima_corrente(p4) \
+#     .set_proxima_corrente(p5) \
+#     .set_proxima_corrente(p6) \
+#     .set_proxima_corrente(p7) \
+#     .set_proxima_corrente(p8)
 p1.corrente(contexto=contexto)
--- src/corrente_pipeline_comentarios/tratramento_comentarios_corrente.py
+++ src/corrente_pipeline_comentarios/tratramento_comentarios_corrente.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pandas as pd
 
 from src.contexto.contexto import Contexto
@@ -11,6 +13,7 @@ class TratamentoComentariosCorrente(Corrente):
         super().__init__()
         self.__banco_analitico = operacoes_banco
         self.__camino_consulta_comentarios = f's3://extracao/youtube/bronze/comentarios_youtube/*/*/comentarios.json'
+        self.__caminho_consulta_resposta_comentarios = f's3://extracao/youtube/bronze/resposta_comentarios_youtube/*/*/*/resposta_comentarios.json'
 
     def __obter_dataset_comentarios(self) -> pd.DataFrame:
         dataframe_comentarios = self.__banco_analitico.consultar_dados('1=1', self.__camino_consulta_comentarios)
@@ -22,7 +25,7 @@ class TratamentoComentariosCorrente(Corrente):
         df_comentarios_final = df_snippet_comentarios[
             ['channelId', 'videoId', 'topLevelComment_id', 'topLevelComment_snippet_textDisplay']]
 
-        df_comentarios_final.rename(
+        df_comentarios_final = df_comentarios_final.rename(
             columns={
                 "channelId": 'id_canal',
                 "videoId": "id_video",
@@ -30,14 +33,53 @@ class TratamentoComentariosCorrente(Corrente):
                 "topLevelComment_snippet_textDisplay": "texto_comentario"
 
             },
-            inplace=True
+
         )
         return df_comentarios_final
 
-    def __obter_dataset_resposta_comentarios(self, dataset: pd.DataFrame):
+    def __obter_dataset_resposta_comentarios(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        dataframe_reposta_comentarios = self.__banco_analitico.consultar_dados(
+            '1=1',
+            self.__caminho_consulta_resposta_comentarios
+        )
+        records: List = dataframe_reposta_comentarios[['snippet', 'id']].fillna(
+            {'snippet': {}}).to_dict(orient='records')
+        df_snippet_resposta_comentarios = pd.json_normalize(records, sep='_')
 
+        df_snippet_resposta_comentarios['id_comentario'] = df_snippet_resposta_comentarios['id'].str.split('.').str[1]
+
+        df_resposta_comentarios_final = pd.merge(
+            dataset[['id_video', 'id_comentario']],
+            df_snippet_resposta_comentarios,
+            left_on='id_comentario',
+            right_on='snippet_parentId',
+            how='inner'
+        )
+        df_resposta_comentarios_final = df_resposta_comentarios_final[
+            ['snippet_channelId', 'id_video', 'id_comentario_x', 'snippet_textDisplay']]
+
+        df_resposta_comentarios_final = df_resposta_comentarios_final.rename(columns={
+            'snippet_channelId': 'id_canal',
+            'videoId': 'id_video',
+            'snippet_textDisplay': 'texto_comentario',
+            'id_comentario_x': 'id_comentario'
+        })
+        return df_resposta_comentarios_final
+
+    @staticmethod
+    def __unir_dataset(**kwargs) -> pd.DataFrame:
+        df_comentarios_final = kwargs['df_comentarios_final']
+        df_resposta_comentarios_final = kwargs['df_resposta_comentarios_final']
+        df_comentarios_tratado_final = pd.concat([df_resposta_comentarios_final, df_comentarios_final])
+        return df_comentarios_tratado_final
 
     def executar_processo(self, contexto: Contexto) -> bool:
         df_comentarios = self.__obter_dataset_comentarios()
-        print(df_comentarios)
+        df_resposta_comentarios = self.__obter_dataset_resposta_comentarios(dataset=df_comentarios)
+        dataset_comentarios_tratado = self.__unir_dataset(
+            df_comentarios_final=df_comentarios,
+            df_resposta_comentarios_final=df_resposta_comentarios
+        )
+        print(dataset_comentarios_tratado)
+
         return True
